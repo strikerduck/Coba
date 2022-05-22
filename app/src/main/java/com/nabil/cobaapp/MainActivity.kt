537dd4ed80c73721dea59ca30371394ce9d60ee2
Nabil Qois <nabilqois91@gmail.com>
2022-05-22 16:14:19 +0700

    adding viewBinding

--- app/src/main/java/com/nabil/cobaapp/MainActivity.kt
+++ app/src/main/java/com/nabil/cobaapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.nabil.cobaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nabil.cobaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
